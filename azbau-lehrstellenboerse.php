8fe59c0be903b03c4f1ac5ff54e022ed9d92a4a9
Wunst <matthiesbe@gmail.com>
2024-09-11 11:24:52 +0200

    Fail to upload files

--- azbau-lehrstellenboerse.php
+++ azbau-lehrstellenboerse.php
@@ -12,12 +12,53 @@
 add_action("admin_menu", "lsb_upload_page");
 
 function lsb_upload_page() {
+  add_settings_section("lsb", "Lehrstellenbörse", null, "lsb");
+
+  add_settings_field(
+    "lsb_file",
+    "Datei",
+    "lsb_file_html",
+    "lsb",
+    "lsb"
+  );
+
+  register_setting("lsb", "lsb_file", "lsb_handle_file_upload");
+
   add_menu_page(
     "Lehrstellenbörse",
     "Lehrstellenbörse",
     "edit_posts",
-    plugin(__FILE__) .. "admin/view.php"
+    "lehrstellenboerse",
+    "lsb_upload_page_html"
   );
+}
+
+function lsb_file_html() {
+  ?>
+    <input type="file" name="lsb_file" />
+    <?php echo get_option("lsb_file"); ?>
+  <?php
+}
+
+function lsb_upload_page_html() {
+  ?>
+  <div class="wrap">
+    <h1>Lehrstellenbörse</h1>
+    <form action="options.php" method="post">
+      <?php
+        settings_fields("lsb");
+
+        do_settings_sections("lsb");
+
+        submit_button();
+      ?>
+    </form>
+  </div>
+  <?php
+}
+
+function lsb_handle_file_upload($option) {
+  header("-Custom-Foobar: " . implode(",", $_FILES));
 }
 
 ?>
