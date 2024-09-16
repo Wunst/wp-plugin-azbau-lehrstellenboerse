b71af7f598002dd26b81780cefaeacaf50b2d7eb
Wunst <matthiesbe@gmail.com>
2024-09-16 14:09:44 +0200

    Implemented table view

--- lsb/render.php
+++ lsb/render.php
@@ -1,8 +1,96 @@
 <table <?php echo get_block_wrapper_attributes(); ?>>
   <tr>
-    <th>Betrieb</th>
-    <th>Ansprechpartner</th>
-    <th>etc.</th>
+    <th>Ausbildungsplätze</th>
+    <th>Ort</th>
+    <th>Betrieb/Ansprechpartner</th>
+    <th>Kontakt</th>
   </tr>
+<?php
+# Spalten, die Ausbildungsplätze enthalten.
+$countCols = array( 
+  "Maurer",
+  "Zimmerer",
+  "Fliesenleger",
+  "Betonbauer",
+  "Straßenbauer",
+  "Kanalbauer",
+  "HBF Maurerarbeiten",
+  "ABF Zimmerarbeiten",
+  "ABF Fliesenarbeiten",
+  "HBF Betonarbeiten",
+  "TBF Straßenbauarbeiten",
+  "TBF Kanalbauarbeiten",
+  "TBF Brunnenbauarbeiten",
+  "Brunnenbauer",
+  "Wärme-, Kälte-, Schallschutzisolierer",
+  "ABF Wärme, Kälte-Schallschutz-Arbeiten"
+);
+
+$data = get_option("lsb_file");
+
+foreach ($data as $row) {
+  # Nur Zeilen mit mind. 1 Ausbildungsplatz darstellen.
+  $render = false;
+  foreach ($countCols as $col) {
+    $cnt = intval($row[$col]);
+    if ($cnt != 0) {
+      $render = true;
+      break;
+    }
+  }
+  if (!$render) continue;
+
+?>
+  <tr>
+    <td><?php
+
+  # Ausbildungsplätze.
+  foreach ($countCols as $col) {
+    $cnt = intval($row[$col]);
+    if ($cnt != 0) {
+      echo $cnt . "x " . $col . "<br/>";
+    }
+  }
+  echo "</td><td>";
+
+  # Ort.
+  $plz = intval($row["PLZ"]);
+  echo $plz . " " . esc_html($row["Ort"]);
+  echo "</td><td>";
+
+  # Betrieb.
+  $url = $row["Internet"];
+  if ($url) {
+    echo '<a href="' . esc_url($url) . '">';
+  }
+  echo esc_html($row["Name 1"]) . "<br/>" . 
+    esc_html($row["Vorname"]) . "<br/>" . 
+    esc_html($row["Name 2"]);
+  if ($url) {
+    echo "</a>";
+  }
+  echo "</td><td>";
+
+  # Kontakt.
+  $tel = $row["Telefon"];
+  if ($tel) {
+    echo 'Tel. <a href="tel:' . $tel . '">' . $tel . "</a><br/>";
+  }
+
+  $mobile = $row["Handy"];
+  if ($mobile) {
+    echo 'Handy <a href="tel:' . $mobile . '">' . $mobile . "</a><br/>";
+  }
+
+  $mail = $row["E-Mail"];
+  if ($mail) {
+    echo 'Email <a href="mailto:' . $mail . '">' . $mail . "</a><br/>";
+  }
+
+    ?></td>
+  </tr>
+<?php
+}
+?>
 </table>
 
