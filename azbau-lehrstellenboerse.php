36ab49237822082da9014127aab94ab0c17a2067
Wunst <matthiesbe@gmail.com>
2024-09-18 11:32:12 +0200

    Require release asset

--- azbau-lehrstellenboerse.php
+++ azbau-lehrstellenboerse.php
@@ -5,9 +5,11 @@
  * Plugin URI: https://github.com/Wunst/wp-plugin-azbau-lehrstellenboerse
  * Author: Ben Matthies
  * Author URI: https://github.com/Wunst
- * Version: 1.0.0
+ * Version: 1.0.1
  * Update URI: false
  * GitHub Plugin URI: Wunst/wp-plugin-azbau-lehrstellenboerse
+ * Primary Branch: main
+ * Release Asset: true
  * Requires at least: 6.2
  * Requires PHP: 8.2
  */
