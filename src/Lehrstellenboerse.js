55449cbc48e0be6c370a7a938ccfe7187435766b
Ben Matthies <matthiesbe@gmail.com>
2025-09-07 23:00:56 +0200

    Start with Tanstack table

--- src/Lehrstellenboerse.js
+++ src/Lehrstellenboerse.js
@@ -3,6 +3,53 @@ import {
   useEffect,
 } from "@wordpress/element"
 
+import {
+  createColumnHelper,
+} from "@tanstack/react-table"
+
+const columnHelper = createColumnHelper()
+
+const columns = [
+  columnHelper.accessor("name", {
+    id: "name",
+    header: () => "Betrieb/Ansprechpartner",
+    cell: info => info.getValue(),
+  }),
+  columnHelper.accessor("plz", {
+    id: "plz",
+    header: () => "PLZ",
+    cell: info => info.getValue(),
+  }),
+  columnHelper.accessor("city", {
+    id: "city",
+    header: () => "Ort",
+    cell: info => info.getValue(),
+  }),
+  columnHelper.group({
+    id: "contact",
+    header: () => "Kontakt",
+    columns: [
+      columnHelper.accessor("contact.phone", {
+        id: "phone",
+        cell: info => <a href="tel:${info.getValue()}">📞${info.getValue()}</a>, 
+      }),
+      columnHelper.accessor("contact.cell", {
+        id: "phone",
+        cell: info => <a href="tel:${info.getValue()}">📱${info.getValue()}</a>,
+      }),
+      columnHelper.accessor("contact.email", {
+        id: "email",
+        cell: info => <a href="mailto:${info.getValue()}">📧${info.getValue()}</a>,
+      }),
+    ],
+  }),
+  columnHelper.group({
+    id: "positions",
+    columns: {
+    },
+  }),
+]
+
 export default function Lehrstellenboerse() {
   const [results, setResults] = useState([])
   useEffect(() => {
@@ -12,5 +59,4 @@ export default function Lehrstellenboerse() {
     })()
     return () => {}
   }, [])
-  console.log(results)
 }
